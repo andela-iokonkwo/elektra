90ad4453c7858d427d00f52c4e7aa4e2ad4b293c
Ikem Okonkwo <ikem.okonkwo@andela.com>
2015-12-13 14:34:26 +0100

    add a base class with call instance method and routes(get, post, put, delete, patch, head) class methods

---
+++ lib/elektra/base.rb
@@ -0,0 +1,39 @@
+module Elektra
+  class Base
+
+    class << self
+
+      def self.route_methods(*methods)
+        Array(methods).each do |method_name|
+          define_method(method_name) do |*args, &block|
+            endpoints[method_name][args[0]] = block
+          end
+        end
+      end
+
+      route_methods :get, :patch, :put, :post, :delete, :head
+
+      def endpoints
+        @endpoints ||= Hash.new { |hash, key| hash[key] = {} }
+      end
+    end
+
+    def call(env)
+      @req = Rack::Request.new(env)
+      path = @req.path_info
+      verb = @req.request_method.downcase.to_sym
+      block_to_execute = self.class.endpoints[verb].fetch(path, nil)
+
+      if block_to_execute
+        response = instance_eval(&block_to_execute)
+        if response.class == String
+          [200, {"Content-Type" => "text/html"}, [response]]
+        else
+          response
+        end
+      else
+        [404, {}, ["This endpoint do not exist"]]
+      end
+    end
+  end
+end
