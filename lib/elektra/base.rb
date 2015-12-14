4f7a76c11528dbe7a1f5a962f91dd582bef28db9
Ikem Okonkwo <ikem.okonkwo@andela.com>
2015-12-14 15:19:36 +0100

    seperate routes method into routes.rb file

--- lib/elektra/base.rb
+++ lib/elektra/base.rb
@@ -1,38 +1,12 @@
 require "rack"
 require "pry"
-require_relative "helpers"
 require_relative "filters"
+require_relative "helpers"
+require_relative "routes"
 
 module Elektra
   class Base
     attr_reader :response, :request
-
-    class << self
-
-      def self.route_methods(*methods)
-        Array(methods).each do |method_name|
-          define_method(method_name) do |*args, &block|
-            pattern = args[0]
-            endpoints[method_name] << [get_path(pattern), block]
-          end
-        end
-      end
-
-      route_methods :get, :patch, :put, :post, :delete, :head
-
-      def endpoints
-        @endpoints ||= Hash.new { |hash, key| hash[key] = [] }
-      end
-
-      def get_path(pattern)
-        placeholders = []
-        pattern.gsub!(/(:\w+)/) do |match|
-          placeholders << $1[1..-1]
-          "([^/?#]+)"
-        end
-        [%r{^#{pattern}$}, placeholders]
-      end
-    end
 
     %w(params body).each do |method_name|
       define_method(method_name) do
---
+++ lib/elektra/routes.rb
@@ -0,0 +1,32 @@
+
+module Elektra
+  class Base
+    attr_reader :response, :request
+
+    class << self
+
+      def self.route_methods(*methods)
+        Array(methods).each do |method_name|
+          define_method(method_name) do |*args, &block|
+            pattern = args[0]
+            endpoints[method_name] << [get_path(pattern), block]
+          end
+        end
+      end
+
+      route_methods :get, :patch, :put, :post, :delete, :head
+
+      def endpoints
+        @endpoints ||= Hash.new { |hash, key| hash[key] = [] }
+      end
+
+      def get_path(pattern)
+        placeholders = []
+        pattern.gsub!(/(:\w+)/) do |match|
+          placeholders << $1[1..-1]
+          "([^/?#]+)"
+        end
+        [%r{^#{pattern}$}, placeholders]
+      end
+    end
+  end
